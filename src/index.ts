51e9e8321ebf2f9f1cc8a28cb7d7e8c784a7e48b
zishanx <zishanalikhan53@gmail.com>
2026-06-11 20:29:49 +0530

    Wrote some function

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+interface Task {
+    id: number
+    title: string
+    completed: boolean
+}
+
+let tasks: Task[] = [];
+
+function addTask(title: string): void {
+    let newTask: Task = {
+        id: tasks.length + 1,
+        title,
+        completed: false
+    }
+
+    tasks.push(newTask)
+}
+
+function completeTask(id: number): void {
+    let findTask: Task | undefined = tasks.find(item => item.id === id)
+    if (findTask) {
+        findTask.completed = true;
+    }
+}
+
+function deleteTask(id: number): void {
+    tasks = tasks.filter(item => item.id !== id);
+}
+
+function listTask(): void {
+    tasks.forEach(item => console.log(item))
+}
+
+addTask("Buy groceries")
+addTask("Learn TypeScript")
+addTask("Play cricket")
+// listTask()
+
+completeTask(2)
+deleteTask(3)
+listTask()
