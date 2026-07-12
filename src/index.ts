0ae29f42e84dcf1cdfa803e609e1ffff4941638b
zishanx <zishanalikhan53@gmail.com>
2026-07-12 21:36:43 +0530

    Done

--- src/index.ts
+++ src/index.ts
@@ -27,10 +27,6 @@ function completeTask(id: number): void {
     saveTasks()
 }
 
-function deleteTask(id: number): void {
-    tasks = tasks.filter(item => item.id !== id);
-    saveTasks()
-}
 
 function listTask(): void {
     tasks.forEach(item => console.log(item))
